8cbce9d3dce81430fddc4ef59765e0d4031c6f65
MilaStankovic <mila.st@elfak.rs>
2025-03-29 22:32:57 +0100

    feat: fetching data from OMDB API with useEffect

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import StarRating from './StarRating';
+
+function Test() {
+  const [movieRating, setMovierating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color='blue' maxRating={10} onSetRating={setMovierating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
+    <StarRating
+      size={24}
+      color='red'
+      className="test"
+      defaultRating={3}
+    />
+    <Test /> */}
   </React.StrictMode>
 );
