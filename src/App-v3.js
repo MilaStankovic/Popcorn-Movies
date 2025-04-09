b20131e605a38283e509ea547b861c2b20b63fb4
MilaStankovic <mila.st@elfak.rs>
2025-04-09 15:52:27 +0200

    feat: finishing app with customHooks

--- src/App-v3.js
+++ src/App-v3.js
@@ -1,8 +1,8 @@
+//BEFORE CREATING CUSTOMHOOKS BUT DIDN'T CREATE THIS VERSION ON TIME SO I HAVE FEW CUSTOM HOOKS ALREADY :(
 import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
 import { useMovies } from "./useMovies";
-import { useLocalStorageState } from "./useLocalStorageState";
-import { useKey } from "./useKey";
+import { useLocalStorageState } from "./useLOcalStorageState";
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -118,11 +118,27 @@ function Logo() {
 function Search({ query, setQuery }) {
   const inputEl = useRef(null)
 
-  useKey("Enter", function () {
-    if (document.activeElement === inputEl.current) return;
-    inputEl.current.focus();
-    setQuery('');
-  });
+  useEffect(function () {
+    function callback(e) {
+      if (document.activeElement === inputEl.current) return;
+
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery('');
+      }
+    }
+
+    document.addEventListener('keydown', callback);
+    return () => document.addEventListener('keydown', callback);
+  },
+    [setQuery]
+  );
+
+  // useEffect(function () {
+  //   const el = document.querySelector('.search');
+  //   console.log(el);
+  //   el.focus();
+  // }, [])
 
   return (
     <input
@@ -280,7 +296,22 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     // setAvgRating(avgRating => (avgRating + userRating) / 2);
   }
 
-  useKey("Escape", onCloseMovie);
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === 'Escape') {
+          onCloseMovie();
+        }
+      }
+
+      document.addEventListener('keydown', callback);
+
+      return function () {
+        document.removeEventListener('keydown', callback);
+      }
+    },
+    [onCloseMovie]
+  );
 
   useEffect(function () {
     async function getMovieDetails() {
